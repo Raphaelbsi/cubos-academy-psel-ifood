46836f832033614bb8300c07cc625ba01c5c0907
Raphael Souza de Oliveira <raphaelsouzadeoliveira@MacBook-Pro-de-Raphael.local>
2021-08-10 18:14:38 -0300

    legal

---
+++ escondeEsconde.js
@@ -0,0 +1,9 @@
+function solucao(numero) {
+  if (numero > 20) {
+    for (var i = 21; i <= numero; i++) {
+      console.log(i);
+    }
+  } else {
+    console.log("VOCE CONSEGUE, MANINHO");
+  }
+}
